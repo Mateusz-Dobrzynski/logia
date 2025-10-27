b7b0813e9b540d6e27d8343133674aa9d4162c5f
Mateusz Dobrzyński <mateusz.dobrzynski64@gmail.com>
2025-10-27 20:04:10 +0100

    feat: szyfr WIP (reprise)

--- solutions/2025-10-22 – szyfr.py
+++ solutions/2025-10-22 – szyfr.py
@@ -11,9 +11,31 @@ tablica_testowa = [
 def odszyfruj(wejscie: str) -> str:
     szerokosc, pozycje, zaszyfrowana_wiadomosc = wejscie.split("\n")
     tablica = robienie_macierzy(zaszyfrowana_wiadomosc, szerokosc)
+    odszyfrowana_wiadomosc = ""
+    pozycje = (pozycje).split(" ")
+    for _ in range(4):
+        for i in range(len(pozycje)):
+            odszyfrowana_wiadomosc += podaj_litere(int(pozycje[i]), tablica)
+        tablica = obracanie(tablica)
+    return odszyfrowana_wiadomosc
+
+
+def robienie_szablonu(pozycje: list, szerokosc: int) -> list[list[bool]]:
+    szablon = [[False for _ in range(szerokosc)] for _ in range(szerokosc)]
+    for i in range(len(pozycje)):
+        szukana_pozycja = pozycje[i] - 1
+        wiersz = szukana_pozycja // szerokosc
+        kolumna = szukana_pozycja % szerokosc
+        szablon[wiersz][kolumna] = True
+    return szablon
+
+
+def przykladanie_szablonu(szablon, tablica) -> str:
+    pass
 
 
 def robienie_macierzy(zaszyfrowana_wiadomosc, szerokosc):
+    szerokosc = int(szerokosc)
     tablica = [[] for _ in range(szerokosc)]
     for i in range(szerokosc):
         for j in range(szerokosc):
@@ -21,25 +43,31 @@ def robienie_macierzy(zaszyfrowana_wiadomosc, szerokosc):
     return tablica
 
 
-def podaj_litere(szukana_pozycja, tablica):
+def podaj_litere(szukana_pozycja: int, tablica: list[list[str]]) -> str:
+    szukana_pozycja -= 1
     wiersz = szukana_pozycja // len(tablica)
     kolumna = szukana_pozycja % len(tablica)
-    return tablica[wiersz][kolumna - 1]
+    litera = tablica[wiersz][kolumna]
+    return litera
 
 
-def obracanie(tablica):
+def obracanie(tablica) -> list[list]:
     obrucona_tablica = [[[] for _ in range(len(tablica))] for _ in range(len(tablica))]
     for i in range(len(tablica)):
+        wiersz = tablica[i]
         for j in range(len(tablica)):
-            obrucona_tablica[i][j] = tablica[j][len(tablica) - i - 1]
+            komorka = wiersz[j]
+            obrucona_tablica[j][len(tablica) - i - 1] = komorka
     return obrucona_tablica
 
+
+obracanie(robienie_macierzy("cxrizboeaxwkdwaaajwwgnasikzlezoy.afa", 6))
 
 assert podaj_litere(4, tablica_testowa) == "i"
 assert (
     odszyfruj("6\n1 4 8 12 15 20 23 30 34\ncxrizboeaxwkdwaaajwwgnasikzlezoy.afa")
-    == "ciekawazabawawszyfrowanie"
+    == "ciekawazabawawszyfrowanie.afa"
 )
 assert robienie_macierzy("cxrizboeaxwkdwaaajwwgnasikzlezoy.afa", 6) == tablica_testowa
 
-assert obracanie([[1, 2], [3, 4]]) == [[2, 4], [1, 3]]
+assert obracanie([[1, 2], [3, 4]]) == [[3, 1], [4, 2]]
