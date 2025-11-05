cbb996bd275986b623e6cbceb0d1296eb9ad382c
Mateusz Dobrzyński <mateusz.dobrzynski64@gmail.com>
2025-11-05 20:01:46 +0100

    feat: gra WIP

--- solutions/2025-10-29 – gra.py
+++ solutions/2025-10-29 – gra.py
@@ -3,7 +3,9 @@ def gra(wejscie: str) -> str:
     # liczenie przejścia małgosi do jasia
     # odwrócić połowę ścieżki
     # wypisać wynik
-    pass
+    malgosia, jas = odwarcanie_polowy_scierzki(scierzka).split()
+    wynik = f"{malgosia}\n{jas}"
+    return wynik
 
 
 def robienie_macierzy(wejscie: str):
@@ -15,6 +17,45 @@ def robienie_macierzy(wejscie: str):
     return macierz
 
 
+def odwarcanie_polowy_scierzki(scierzka):
+    dlugosc_scierzki_malgosia = len(scierzka)
+    dlugosc_scierzki_malgosia = dlugosc_scierzki_malgosia // 2
+    if len(scierzka) % 2 == 0:
+        dlugosc_scierzki_jas = dlugosc_scierzki_malgosia
+    else:
+        dlugosc_scierzki_jas = dlugosc_scierzki_malgosia + 1
+        dlugosc_scierzki_malgosia += 1
+    malgosia = scierzka[0:dlugosc_scierzki_malgosia]
+    jas = list(scierzka[dlugosc_scierzki_jas : len(scierzka)])
+    jas.reverse()
+    jas = "".join(jas)
+    jas = jas.translate(str.maketrans("DGLP", "GDPL"))
+    return malgosia, jas
+
+
+def szukanie_litery(wejscie, szukana_litera):
+    szerokosc = int(len(wejscie) ** 0.5)
+    litera_poz = wejscie.index(szukana_litera)
+    wiersz = litera_poz // szerokosc
+    kolumna = litera_poz % szerokosc
+    return wiersz, kolumna
+
+
+def liczenie(macierz: list[list[str]], wejscie):
+    start = szukanie_litery(wejscie, "M")
+    koniec = szukanie_litery(wejscie, "J")
+    pozycja = 0
+    wynik = ""
+    krok = ""
+
+
+assert liczenie(robienie_macierzy("MXSSSXXSSSXSXSSJ"), "MXSSSXXSSSXSXSSJ") == "DDPDP"
+
+assert szukanie_litery("MXSXSXXSSSXSXSJX", "M") == (0, 0)
+assert szukanie_litery("MSXJ", "J") == (1, 1)
 assert robienie_macierzy("XXSS") == [["X", "X"], ["S", "S"]]
+
+assert odwarcanie_polowy_scierzki("GDPDLP") == ("GDP", "LPG")
+assert odwarcanie_polowy_scierzki("GDPDLPL") == ("GDPD", "PLP")
 
 # assert gra("MXSXSXXSSSXSXSJX") == "DDP\nLG"
