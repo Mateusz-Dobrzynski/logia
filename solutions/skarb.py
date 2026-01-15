cb2b05c5ae3ab186100215f4967149e4345f1b82
Mateusz Dobrzyński <mateusz.dobrzynski64@gmail.com>
2026-01-15 20:07:39 +0100

    skarb WIP

---
+++ solutions/skarb.py
@@ -0,0 +1,19 @@
+# 2026-E02
+
+
+komnaty = {
+    "A": ["G", "B", "C"],
+    "B": ["E", "D", "A"],
+    "C": ["H", "A", "D"],
+    "D": ["B", "F", "C"],
+    "F": ["E", "H", "D"],
+    "G": ["F", "E", "H"],
+    "H": ["F", "G", "C"],
+}
+
+
+def gdzie_jest_skarb(lista_krokow: str) -> str:
+    pass
+
+
+assert gdzie_jest_skarb("LLPPL") == "C"
